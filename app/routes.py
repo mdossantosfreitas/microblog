24ca32cdecb3880ed820b720815efbf289d69c33
Matheus dos Santos Freitas <mat.santos.freitas@gmail.com>
2020-01-26 21:38:41 -0300

    Add templates to Flask app

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World"
+    user = { 'username' : 'Miguel'}
+    posts =  [
+        {
+            'author' : {'username' : 'Matheus dos Santos Freitas'},
+            'body' : 'Hello Guys, first post'
+        },
+        {
+            'author' : {'username' : 'Raul Borges'},
+            'body' : 'Parasite best movie'
+        }
+    ]
+    return render_template('index.html', title='Home', user = user, posts = posts)
