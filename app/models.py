64516427fffb8a7d96850a8c05672cbc8d78334b
Matheus dos Santos Freitas <mat.santos.freitas@gmail.com>
2020-05-04 22:39:01 +0200

    Add follow/unfollow

--- app/models.py
+++ app/models.py
@@ -5,6 +5,21 @@ from flask_login import UserMixin
 from app import login
 from hashlib import md5
 
+##since it is an auxiliary table, it is not defined as a model
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -13,7 +28,9 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
-
+    followed = db.relationship('User', secondary=followers, primaryjoin=(followers.c.follower_id == id),
+                               secondaryjoin = (followers.c.followed_id == id),
+                               backref = db.backref('followers', lazy='dynamic'), lazy='dynamic')
     def __repr__(self):
         return '<User {}'.format(self.username)
 
@@ -28,14 +45,22 @@ class User(UserMixin, db.Model):
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
 
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        own_posts = Post.query.filter_by(user_id = self.id)
+        posts_followed = Post.query.join(followers, (Post.user_id == followers.c.followed_id)).filter(followers.c.follower_id == self.id)
+        return posts_followed.union(own_posts).order_by(Post.timestamp.desc())
 
 @login.user_loader ##the user_loader of the LoginManager will run the def user_loader function
 def user_loader(id): ##goes to db and return the user with this id
